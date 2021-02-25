e4f5aee8828436b25f3440b3945c4ff0b6d1153e
faust7605 <syd332600@gmail.com>
2021-02-25 11:32:49 +0900

    [server]PATCH DB 및 typeORM 환경 구축2(PATCH)(3h)

--- src/router/contents.ts
+++ src/router/contents.ts
@@ -1,11 +1,47 @@
 import express, { Request, Response, NextFunction } from "express";
-import contents from "../controllers/controllersContents";
-
+import { createConnection } from "typeorm";
+import {Users} from "../entity/Users";
+import controller from "../controllers/controllersContents"
 const router = express.Router();
 
-router.get("/",(req: Request, res: Response, next: NextFunction) => {
+/* test */
+
+createConnection().then (async connection => {
+    const userRepository = connection.getRepository(Users);
+
+    
+router.get("/test",(req: Request, res: Response, next: NextFunction) => {
+    //contents.userInfo
+    //res.send(contents.userInfo)
+    res.json("testpage")
+})
+
+router.get("/find-testpage",controller.testpage)
+
+router.get("/findOne-testpage/:id",async (req: Request, res: Response, next: NextFunction) => {
+    console.log("findOne test") 
+    //http://localhost:4000/contents/findOne-testpage/2 <--- id 2 deleted
+
+        const results = await userRepository.findOne(req.params.id);
+        //const results = await userRepository.delete(req.params.id);   <--- delete
+        return res.send(results);
 
 })
+
+router.delete("/delete/:id", async (req: Request, res: Response) => {
+    console.log("deleted")
+    const userRepository = connection.getRepository(Users);
+    const results = await userRepository.delete(req.params.id);
+    return res.send(results);
+});
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
 
 router.post("/ccontent",(req: Request, res: Response, next: NextFunction) => {
     res.send("ccontent");
@@ -47,5 +83,5 @@ router.post("/calendar",(req: Request, res: Response, next: NextFunction) => {
     res.send("calendar");
 })
 
-
+})
 export = router;
