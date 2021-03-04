59d1a3987c856077bfd7df3919f76bef4397e8b7
dlfjsld1 <ltk900216@gmail.com>
2021-03-04 22:15:37 +0900

    [Server] 일기쓰기 - POST ImageUpload[ADD][1h]

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
 import "reflect-metadata";
-import { createConnection, getRepository } from "typeorm";
+import { createConnection } from "typeorm";
 import contents from "./router/contents";
 import users from "./router/users";
-import { Users } from "./entity/Users";
 require("dotenv").config();
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
@@ -26,6 +25,31 @@ const config: any = {
     subscribersDir: "src/subscriber",
   },
 };
+/******************multer setting***********************/
+const multer = require('multer');
+const path = require('path'); //@types가 없었다 작동 되는지 확인 필요
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_REGION,
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'royaldiarymulter',
+    acl: 'public-read',
+    key: function (req: Request, file: any, cb: Function) {//! file의 타입은 일단 any로 했지만 뭘 해야 하는지?
+      let extension: string = path.extname(file.originalname); //! path.extname()은 확장자를 가져온다. originalname은 뭘 의미하는지?
+      cb(null, Date.now().toString() + extension);
+    },
+  }),
+  limits: { fieldSize: 5 * 1024 * 1024 },//파일사이즈 제한
+});
+/*****************************************/
+
 createConnection(config)
   .then(() => {
     console.log("typeorm connected");
@@ -44,9 +68,29 @@ app.use(
   })
 );
 
-app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello world!");
 });
+
+/*********************multercode********************/
+declare global { // req.file.location 때문에 추가한 타입 선언
+  namespace Express {
+      namespace Multer {
+          interface File {
+              location: string;
+          }
+      }
+  }
+}
+app.post('/image', upload.single('img'), async (req, res) => {
+  try {
+    console.log('req.file: ', req.file.location);
+    res.status(200).send({ imgUrl: req.file.location });
+  } catch (err) {
+    res.status(500).send('err');
+  }
+});
+/*****************************************/
 
 app.use("/contents", contents);
 app.use("/users", users);
