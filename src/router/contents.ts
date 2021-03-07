b91c1136a6365d40f7fc580c290a3a9677d4bf22
dlfjsld1 <ltk900216@gmail.com>
2021-03-07 17:09:02 +0900

    deactive developing contents router api[PATCH]

--- src/router/contents.ts
+++ src/router/contents.ts
@@ -6,29 +6,29 @@ const router = express.Router();
 
 router.post("/ccontent", controller.postCcontet);
 
-router.get("/contents", controller.getContents);
+// router.get("/contents", controller.getContents);
 
 router.get("/content", controller.getContent);
 
-router.patch("/ucontent", controller.patchUcontent);
+// router.patch("/ucontent", controller.patchUcontent);
 
 router.delete("/dcontent", controller.delDcontent);
 
 router.post("/comment", controller.postComment);
 
-router.get(
-  "/publiccontents",
-  (req: Request, res: Response, next: NextFunction) => {
-    res.send("publiccontents");
-  }
-);
+// router.get(
+//   "/publiccontents",
+//   (req: Request, res: Response, next: NextFunction) => {
+//     res.send("publiccontents");
+//   }
+// );
 
-router.patch("/ucomment", controller.patchUcomment);
+// router.patch("/ucomment", controller.patchUcomment);
 
 router.delete("/dcomment",controller.delDcomment);
 
-router.post("/calendar", (req: Request, res: Response, next: NextFunction) => {
-  res.send("calendar");
-});
+// router.post("/calendar", (req: Request, res: Response, next: NextFunction) => {
+//   res.send("calendar");
+// });
 
 export = router;
